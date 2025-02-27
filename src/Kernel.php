4309878c6ddddab6b92347a3271c20aa80438ded
Kaz van Wel <info@kiksaus.nl>
2025-02-27 05:52:08 +0100

    Require app to have DEFAULT_EMAIL_FROM variable set

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,13 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        if (empty($_ENV['DEFAULT_EMAIL_FROM'])) {
+            throw new \RuntimeException('Required DEFAULT_EMAIL_FROM variable is missing');
+        }
+    }
 }
