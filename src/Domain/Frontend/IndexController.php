16306b6ce64978d0246244936d029438a22b3fa8
Kaz van Wel <info@kiksaus.nl>
2026-04-24 08:45:57 +0200

    Refactor `IndexController` to use `Request` directly, replace `NotFoundHttpException` with `createNotFoundException`, and update route and template references.

--- src/Domain/Frontend/IndexController.php
+++ src/Domain/Frontend/IndexController.php
@@ -6,9 +6,8 @@ use KikCMS\Entity\Page\PageRepository;
 use KikCMS\Entity\Page\Path\PathService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -17,14 +16,13 @@ class IndexController extends AbstractController
     public function __construct(
         private readonly TranslatorInterface $translator,
         private readonly PathService $pathService,
-        private readonly RequestStack $requestStack,
         private readonly PageRepository $pageRepository,
     ) {}
 
-    #[Route('/')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(Request $request): Response
     {
-        $locale = $this->requestStack->getCurrentRequest()->getLocale();
+        $locale = $request->getLocale();
         $page   = $this->pageRepository->findOneBy(['identifier' => FrontendConfig::DEFAULT_IDENTIFIER]);
 
         return $this->render('theme/templates/default.twig', [
@@ -34,12 +32,12 @@ class IndexController extends AbstractController
     }
 
     #[Route('/{path}', name: 'page', requirements: ['path' => '[a-z0-9-/]+'], priority: -1)]
-    public function page(string $path): Response
+    public function page(string $path, Request $request): Response
     {
-        $locale = $this->requestStack->getCurrentRequest()->getLocale();
+        $locale = $request->getLocale();
 
         if ( ! $page = $this->pathService->getPageByPath($path, $locale)) {
-            throw new NotFoundHttpException();
+            throw $this->createNotFoundException();
         }
 
         return $this->render('theme/templates/default.twig', [
