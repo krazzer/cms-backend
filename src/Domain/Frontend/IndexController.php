d657a74aeebfc194735a9b005fd406d3bb3c9b6d
Kaz van Wel <info@kiksaus.nl>
2026-09-11 11:19:03 +0200

    Add multicheckbox styling

--- src/Domain/Frontend/IndexController.php
+++ src/Domain/Frontend/IndexController.php
@@ -9,6 +9,7 @@ use KikCMS\Entity\Page\Renderer\GlobalVariables\GlobalVariableResolver;
 use KikCMS\Entity\Page\Renderer\PageRendererResolver;
 use KikCMS\Entity\Page\Renderer\RenderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -67,9 +68,35 @@ class IndexController extends AbstractController
         $params = array_replace_recursive(['lang' => $request->getLocale()], $globals, $result->context);
 
         $form = $this->createFormBuilder()
-            ->add('name', TextType::class, ['attr' => ['placeholder' => 'Naam'], 'constraints' => [new NotBlank()], 'label' => false])
-            ->add('email', EmailType::class, ['label' => false, 'attr' => ['placeholder' => 'E-mail adres'], 'constraints' => [new NotBlank(), new Email()]])
-            ->add('message', TextareaType::class, ['label' => false, 'attr' => ['placeholder' => 'Bericht', 'rows' => 5],'constraints' => [new NotBlank()]])
+            ->add('name', TextType::class, [
+                'attr'        => ['placeholder' => 'Naam'],
+                'constraints' => [new NotBlank()],
+                'label'       => false
+            ])
+            ->add('email', EmailType::class, [
+                'label'       => false,
+                'attr'        => ['placeholder' => 'E-mail adres'],
+                'constraints' => [new NotBlank(), new Email()]
+            ])
+            ->add('message', TextareaType::class, [
+                'label'       => false,
+                'attr'        => ['placeholder' => 'Bericht', 'rows' => 5],
+                'constraints' => [new NotBlank()]
+            ])
+            ->add('type', ChoiceType::class, [
+                'label'            => false,
+                'placeholder'      => 'Pick an option',
+                'placeholder_attr' => ['disabled' => true],
+                'choices'          => ['Option 1' => 1, 'Option 2' => 2],
+                'constraints'      => [new NotBlank()]
+            ])
+            ->add('checkbox', ChoiceType::class, [
+                'label'       => 'Kies een opties',
+                'choices'     => ['Option 1' => 1, 'Option 2' => 2, 'Option 3 long' => 3],
+                'multiple'    => true,
+                'expanded'    => true,
+                'constraints' => [new NotBlank()],
+            ])
             ->add('send', SubmitType::class, ['label' => 'Versturen'])
             ->getForm();
 
