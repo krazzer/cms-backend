eb7f2013b0cb54b702b1e8e26d9966f432d53e05
Kaz van Wel <info@kiksaus.nl>
2026-09-18 10:41:37 +0200

    Change label

--- src/Domain/FrontendForm/ContactFormType.php
+++ src/Domain/FrontendForm/ContactFormType.php
@@ -32,7 +32,7 @@ class ContactFormType extends MailFormType
                 'attr'        => ['placeholder' => $this->translator->trans('form.contact.message'), 'rows' => 8],
                 'constraints' => [new NotBlank()]
             ])
-            ->add('spamblock', SpamBlockType::class)
+            ->add('check', SpamBlockType::class)
             ->add('send', SubmitType::class, ['label' => $this->translator->trans('form.contact.send')]);
     }
 
